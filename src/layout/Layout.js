fd40177333070992acafff8774396e2cf76d72c1
taniayunita <tania.yunita@wgs-id.com>
2022-11-26 19:57:00 +0700

    active sidebar

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
+
 export default function Layout({ children }) {
   const menus = [
     { name: "Roadmap", icons: "icon" },
     { name: "Get Started", icons: "icon" },
     { name: "Home", icons: "icon" },
   ];
+
+  const [active, setActive] = useState(0);
+
   return (
     <div className="container">
       <div className="sidebar">
-        <img className="logo" src="" alt="Aztan" />
+        <div className="logo">
+          <img src="" alt="Aztan" />
+        </div>
         <ul>
           {menus.map((item, index) => (
-            <li key={index}>
+            <li
+              key={index}
+              className={`${active === index && "active"}`}
+              onClick={() => setActive(index)}
+            >
               <div className="icon">{item.icons}</div>
               <div className="name">{item.name}</div>
             </li>

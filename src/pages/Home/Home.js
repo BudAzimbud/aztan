1ba281c33abdee44b5d00b87c24a808f85fee051
taniayunita <tania.yunita@wgs-id.com>
2022-11-26 17:49:23 +0700

    add sidebar

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,28 @@ import React from "react";
 import "./style.scss";
 import AppImage from "../../assets/images/app-image.png";
 export default function Home() {
+  const menus = [
+    {name : 'Roadmap', icons: 'icon'},
+    {name : 'Get Started', icons: 'icon'},
+    {name : 'Home', icons: 'icon'},
+  ]
+
   return (
     <div className="container">
-      <div className="container">
-        <img src={AppImage} alt="slider app" className="image" />
+      <div className="sidebar">
+        <img className="logo" src="" alt="Aztan"/>
+        <ul>
+          {menus.map((item, index) => (
+            <li key={index}>
+              <div className="icon">{item.icons}</div>
+              <div className="name">{item.name}</div>
+            </li>
+          ))}
+        </ul>
       </div>
+      <div className="main-content">
+        <img src={AppImage} alt="slider app" className="image" />
+      
       <div className="header">
         <h1>Aztan Reactjs</h1>
         <p>slug about this page</p>
@@ -20,6 +37,7 @@ export default function Home() {
         <span>50 Components + </span>
         <span>50 Components + </span>
         <span>50 Components + </span>
+      </div>
       </div>
     </div>
   );

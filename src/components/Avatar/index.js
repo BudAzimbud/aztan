0ee13783be916006a9810f2e263f7dbbebc52205
azim <budazimbud@gmail.com>
2022-11-30 17:06:52 +0700

    add avatar image

---
+++ src/components/Avatar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+function Avatar({ src, size = 50 }) {
+  console.log(src);
+  return (
+    <img
+      src={src}
+      style={{
+        borderRadius: "50%",
+        border: "1px solid black",
+      }}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+export default Avatar;

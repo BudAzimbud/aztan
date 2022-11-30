c2284da5f9a9d4796f85192fc25eb79706b223f2
MamankIrfa <irsyad.f2004@gmail.com>
2022-11-30 17:47:53 +0700

    update button Theme

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,36 +1,35 @@
-import React, { useState } from "react";
-import "./style.scss";
-import Logo from "../assets/images/aztan-logo.png";
-import { menus } from "../navigation/vertical";
+import React, { useState } from 'react'
+import './style.scss'
+import Logo from '../assets/images/aztan-logo.png'
+import { menus } from '../navigation/vertical'
+import Theme from '../components/Theme/Theme.js'
 export default function Layout({ children }) {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState(0)
 
   return (
-    <div className="container">
-      <div className="sidebar">
-        <div className="logo">
-          <img src={Logo} alt="Aztan" width="60%" />
-          <div className="btn-side">
+    <div className='container'>
+      <div className='sidebar'>
+        <div className='logo'>
+          <img src={Logo} alt='Aztan' width='60%' />
+          <div className='btn-side'>
             <button>small</button>
           </div>
         </div>
         <ul>
           {menus.map((item, index) => (
-            <li
-              key={index}
-              className={`${active === index && "active"}`}
-              onClick={() => setActive(index)}
-            >
-              <div className="icon">{item.icon}</div>
-              <div className="name">{item.name}</div>
+            <li key={index} className={`${active === index && 'active'}`} onClick={() => setActive(index)}>
+              <div className='icon'>{item.icon}</div>
+              <div className='name'>{item.name}</div>
             </li>
           ))}
         </ul>
       </div>
-      <div className="main-content">
-        <div className="navbar">navbar</div>
-        <div style={{ marginTop: "80px" }}>{children}</div>
+      <div className='main-content'>
+        <div className='navbar'>
+          <Theme />
+        </div>
+        <div style={{ marginTop: '80px' }}>{children}</div>
       </div>
     </div>
-  );
+  )
 }

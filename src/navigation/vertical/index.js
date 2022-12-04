a62de38363846027f55e346acf38db8fc7cdd922
azim <budazimbud@gmail.com>
2022-12-04 11:07:24 +0700

    make navigation about us third on list

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -2,12 +2,12 @@ import { AiOutlineHome, AiOutlineDownload } from "react-icons/ai";
 import { BsStackOverflow, BsFillInfoCircleFill } from "react-icons/bs";
 const menus = [
   { name: "Home", icon: <AiOutlineHome size={22} />, link: "/" },
+  { name: "Roadmap", icon: <BsStackOverflow size={22} />, link: "/roadmap" },
   {
     name: "About Us",
     icon: <BsFillInfoCircleFill size={22} />,
     link: "/about",
   },
-  { name: "Roadmap", icon: <BsStackOverflow size={22} />, link: "/roadmap" },
   {
     name: "Get Started",
     icon: <AiOutlineDownload size={22} />,

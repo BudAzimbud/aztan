f45a86d4600977375c87ceea5cd3f72620f44a85
taniayunita <tania.yunita@wgs-id.com>
2022-12-14 11:26:11 +0700

    replace styling avatar to external

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,19 +1,10 @@
 import React from "react";
+import "./index.scss";
+
 function Avatar({ src, size = 50 }) {
   console.log(src);
   return (
-    <img
-      src={src}
-      style={{
-        borderRadius: "50%",
-        boxShadow: '0px 8px 30px -10px rgba(0,0,0,0.6)',
-        objectFit: 'cover'
-
-      }}
-      alt="Avatar"
-      width={size}
-      height={size}
-    />
+    <img src={src} className="avatar" alt="Avatar" width={size} height={size} />
   );
 }
 

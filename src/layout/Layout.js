a2fc18cc0cbecba8367206afc7a7c586903f00c3
azim <budazimbud@gmail.com>
2023-01-22 12:22:32 +0700

    update button humberger

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,23 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import { menus } from "../navigation/vertical";
-import Theme from '../components/Theme'
+import Theme from "../components/Theme";
 import Sidebar from "../components/Sidebar";
 
 import "./style.scss";
 
 export default function Layout({ children }) {
-  const [smallSidebar,setSmallSidebar] = useState(false)
+  const [smallSidebar, setSmallSidebar] = useState(false);
 
-    const handleSidebarSize = () => {
-        setSmallSidebar(!smallSidebar)
-    }
+  const handleSidebarSize = () => {
+    setSmallSidebar(!smallSidebar);
+  };
 
   return (
-    <div className='container'>
-      <Sidebar smallSidebar={smallSidebar} handleSidebarSize={handleSidebarSize} menus={menus}/>
-      <div className={`${smallSidebar? 'main-content-small' : 'main-content'}`}>
-        <div className='navbar'>
+    <div className="container">
+      <Sidebar
+        smallSidebar={smallSidebar}
+        handleSidebarSize={handleSidebarSize}
+        menus={menus}
+      />
+      <div
+        className={`${smallSidebar ? "main-content-small" : "main-content"}`}
+      >
+        <div className="navbar">
           <Theme />
         </div>
         <div className="content">{children}</div>

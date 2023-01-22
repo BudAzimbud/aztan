549953a1ac4d0e0f2294af3460972c9703e104c1
taniayunita <tania.yunita@wgs-id.com>
2023-01-22 15:06:38 +0700

    humberger and screen setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: { min: "0px", max: "768px" },
+      // => @media (min-width: 0px and max-width: 479px) { ... }
+
+      tablet: { min: "768px", max: "991px" },
+      // => @media (min-width: 480px and max-width: 991px) { ... }
+
+      netbook: { min: "992px", max: "1024px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      pc: { min: "1025px" },
+      // => @media (min-width: 1025px) { ... }
+    },
+    extend: [],
   },
   plugins: [],
 };

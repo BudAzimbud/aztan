b88c325fb71485e27e02ad1bab97dab38a2aa67a
Azim <budazimbud@gmail.com>
2023-02-15 15:25:19 +0700

    add roadmap page

--- src/pages/Roadmap/index.js
+++ src/pages/Roadmap/index.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import  './style.scss'
 function Roadmap() {
-  return <div>Roadmap</div>;
+  return (
+    <article class={"wrapper-border-animation"}>
+      <h3>Border Animation</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nostrum quam
+        mollitia sit aliquam omnis nisi neque nulla, ipsa obcaecati eveniet
+        incidunt doloribus quaerat at totam nemo exercitationem, officiis
+        quisquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Necessitatibus, quisquam perferendis
+      </p>
+      <span class="top"></span>
+      <span class="right"></span>
+      <span class="bottom"></span>
+      <span class="left"></span>
+    </article>
+  );
 }
 
 export default Roadmap;

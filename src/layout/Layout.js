f50d762e44ae6b27f3b04e6a232a0ae2d37247b0
Azim <budazimbud@gmail.com>
2023-02-15 20:26:40 +0700

    add custom hooks height

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -7,7 +7,35 @@ import Sidebar from "../components/Sidebar";
 
 import "./style.scss";
 
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
 export default function Layout({ children }) {
+  const size = useWindowSize();
   const [smallSidebar, setSmallSidebar] = useState(false);
   const [showHumberger, setShowHumberger] = useState(0);
   const [showSidebar, setShowSidebar] = useState("hide-sidebar");
@@ -28,10 +56,7 @@ export default function Layout({ children }) {
   };
   useEffect(()=>{
     setSmallSidebar(false)
-
-   
-    
-  },[])
+  },[size])
 
   return (
     <div className="wrapper">
